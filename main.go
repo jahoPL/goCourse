0c4b02fcdfddd525413ec2ae95f48bb448588ebb
Piotr Kołodyński <hehlolrofl@gmail.com>
2020-10-23 19:16:23 +0200

    chanel examples

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-	"github/Ko4s/goCourse/topic3"
+	"github/Ko4s/goCourse/topic4"
 	"log"
+	"time"
 )
 
 func main() {
-	h := topic3.NewHasher("in", "out")
+	// Go routines example
+	// go topic4.InfinitePrintFunction("Bump") //odpalenie gorutyny
+	// time.Sleep(time.Second) //czekam 1s, cały program kończy się kiedy skonczy się funkcja main
 
-	err := h.ReadLines()
-	handleError(err)
-
-	err = h.HashLines()
-	handleError(err)
-
-	err = h.SaveToFile()
-	handleError(err)
+	//Channele
+	c := make(chan int)
+	go topic4.SetChanel(c)
+	go topic4.GetChannelValue(c)
+	time.Sleep(time.Second * 10)
 }
 
 func handleError(err error) {
---
+++ topic4/links.md
@@ -0,0 +1,3 @@
+[Goroutines & Channels](https://medium.com/@manus.can/golang-tutorial-goroutines-and-channels-c2cd491f77ab)
+[Goroutines](https://gobyexample.com/goroutines)
+[Channels](https://tour.golang.org/concurrency/2)
